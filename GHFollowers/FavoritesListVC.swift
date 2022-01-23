42a1e19f798e4aa1fc85f3eeeaccd7d412b4a7c9
Jonathan Grajkowski <Jonathan Grajkowski>
2022-01-22 20:05:59 -0500

    first commit

---
+++ GHFollowers/FavoritesListVC.swift
@@ -0,0 +1,17 @@
+//
+//  FavoritesListVC.swift
+//  GHFollowers
+//
+//  Created by Jonathan Grajkowski on 2021-11-29.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBlue
+    }
+}
---
+++ GHFollowers/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  GHFollowers
+//
+//  Created by Jonathan Grajkowski on 2021-11-29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemRed
+    }
+}
+
